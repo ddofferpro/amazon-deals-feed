cdd8bd3809bca2389c9443d035796f87c2559838
ddofferpro <ddoffer143@gmail.com>
2025-04-16 18:41:34 +0530

    Update tweets_from_rss.py

--- tweets_from_rss.py
+++ tweets_from_rss.py
@@ -18,11 +18,12 @@ jobs:
           python-version: 3.10
 
       - name: Install dependencies
-        run: pip install -r requirements.txt
+        run: pip install -r amazon-deals-feed/requirements.txt
 
       - name: Run Tweet Script
-        working-directory: ./  # 👈 Ensures it's the root where your script lives
-        run: python tweet_from_rss.py
+        run: |
+          cd amazon-deals-feed
+          python tweet_from_rss.py
         env:
           TWITTER_API_KEY: ${{ secrets.TWITTER_API_KEY }}
           TWITTER_API_SECRET: ${{ secrets.TWITTER_API_SECRET }}
